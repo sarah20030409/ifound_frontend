db5d13423ead7daab8aac68bfd352ec5c3cab410
sarah20030409 <101374417+sarah20030409@users.noreply.github.com>
2024-07-15 20:28:10 +0800

    Update

---
+++ src/components/HelloComp.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+const Props: any = defineProps({
+  name: String
+})
+</script>
+
+<template>
+  <h1>Hello! {{ name }}, this is IFound frontend !</h1>
+</template>
