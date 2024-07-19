544aa38fe4075d65b9ffb4cd927014b7e31bc4a8
sarah20030409 <101374417+sarah20030409@users.noreply.github.com>
2024-07-19 13:17:46 +0800

    Tags style setting

---
+++ src/pages/testPage/components/HelloComp.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+const _props: any = defineProps({
+  name: String
+})
+</script>
+
+<template>
+  <div class="bg-tagRed border-4 border-tagRedText">
+    <h1 class="text-3xl font-bold p-4 mb-[10px] text-tagRedText lg:text-tagOrangeText">
+      Hello! {{ name }}, this is IFound frontend !
+    </h1>
+  </div>
+
+  <div class="tagContainer">
+    <div class="TagRed">
+      <p>Red</p>
+    </div>
+
+    <div class="TagOrange">
+      <p>Orange</p>
+    </div>
+
+    <div class="TagGreen">
+      <p>Green</p>
+    </div>
+
+    <div class="TagCyan">
+      <p>Cyan</p>
+    </div>
+
+    <div class="TagBlue">
+      <p>Blue</p>
+    </div>
+
+    <div class="TagPurple">
+      <p>Purple</p>
+    </div>
+
+    <div class="TagMagenta">
+      <p>Magenta</p>
+    </div>
+
+    <div class="TagPink">
+      <p>Pink</p>
+    </div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        ifoundRed: '#DE4D4A',
+        ifoundGary: '#D9D3D5',
+        ifoundDarkGray: '#615F5F',
+        ifoundBlue: '#1D6AA2',
+        //====Tag Color=====
+        tagRed: '#FFCBC9',
+        tagRedText: '#CE0E0A',
+        tagOrange: '#FFE0C9',
+        tagOrangeText: '#ED6E13',
+        tagYellow: '#FFF59A',
+        tagYellowText: '#938500',
+        tagGreen: '#DEFFC9',
+        tagGreenText: '#46B900',
+        tagCyan: '#C9FFF9',
+        tagCyanText: '#0BCCB5',
+        tagBlue: '#C9E9FF',
+        tagBlueText: '#23A3FF',
+        tagPurple: '#CBC9FF',
+        tagPurpleText: '#4844FF',
+        tagMagenta: '#F8C9FF',
+        tagMagentaText: '#E12CFF',
+        tagPink: '#FFC9DA',
+        tagPinkText: '#FF3270'
+        //=========
+      },
+      screens: {
+        sm: '410px',
+        md: '500px',
+        lg: '800px'
+      }
+    }
+  },
+  plugins: []
+}
