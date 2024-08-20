420cf6f23c9dc13319c0552fd3b5f0951144bd85
Your Name <a0908647929@gmail.com>
2024-08-20 15:20:15 +0800

    Achievement page is done

--- src/global/footer/RouterSwitch.ts
+++ src/global/footer/RouterSwitch.ts
@@ -4,13 +4,15 @@ import SignupLogin from '@/pages/signupLoginPage/SignupLogin.vue'
 import UploadPage from '@/pages/uploadPage/UploadPage.vue'
 import UserSelfListPage from '@/pages/lostListPage/userSelfListPage/UserSelfLostList.vue'
 import LostInfoPage from '@/pages/lostInfoPage/LostInfoPage.vue'
+import AchievementPage from '@/pages/achievementPage/AchievementPage.vue'
 
 const routes = [
   { path: '/', component: LostList },
   { path: '/signupLogin', component: SignupLogin },
   { path: '/uploadItem', component: UploadPage },
   { path: '/userSelfListPage', component: UserSelfListPage },
-  { path: '/lostInfoPage', component: LostInfoPage }
+  { path: '/lostInfoPage', component: LostInfoPage },
+  { path: '/achievementPage', component: AchievementPage }
 ]
 
 const router = createRouter({
